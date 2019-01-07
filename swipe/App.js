d3c9f0c498cc4b31d216769d1be32eda236d9dc0
sobiwan <smobrien914@gmail.com>
2019-01-07 13:12:33 -0600

    prettier init swip

--- swipe/App.js
+++ swipe/App.js
@@ -16,5 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
-  },
+  }
 });
--- swipe/src/Ball.js
+++ swipe/src/Ball.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { StyleSheet, Animated, View } from 'react-native';
 
 export default class Ball extends React.Component {
-    componentWillMount(){
-        this.position = new Animated.ValueXY(0,0);
-        Animated.spring(this.position, {
-            toValue: { x: 200, y: 500 }
-        }).start();
-    }
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, {
+      toValue: { x: 200, y: 500 }
+    }).start();
+  }
 
   render() {
     return (
-        <Animated.View style={this.position.getLayout()}>
-            <View style={styles.ball} />
-        </Animated.View>
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
     );
   }
 }
@@ -25,5 +25,5 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     borderWidth: 30,
     borderColor: 'black'
-  },
+  }
 });
