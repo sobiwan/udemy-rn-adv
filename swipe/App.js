ba8ba1f7e88543e385123caf0b2dc65a133b0eff
sobiwan <smobrien914@gmail.com>
2019-01-07 22:45:00 -0600

    swipe deck complete

--- swipe/App.js
+++ swipe/App.js
@@ -6,12 +6,30 @@ import Deck from './src/Deck';
 export default class App extends React.Component {
   renderCard(item) {
     return (
-      <Card key={item.id} title={item.text} image={{ uri: item.uri }}>
+      <Card
+        key={item.id}
+        title={item.text}
+        image={{ uri: item.uri }}
+      >
         <Text>I can customize this further</Text>
         <Button
           icon={{ name: 'code' }}
           backgroundColor="#03A9F4"
-          title="view now"
+          title="Details"
+        />
+      </Card>
+    );
+  }
+
+  renderNoMoreCards() {
+    return (
+      <Card title="Thats All Folks">
+        <Text style={{ marginBottom: 10 }}>
+          No Cards for YOU!
+        </Text>
+        <Button
+          backgroundColor="#03A9F4"
+          title="Load more!"
         />
       </Card>
     );
@@ -24,8 +42,9 @@ export default class App extends React.Component {
           renderCard={this.renderCard}
           renderNoMoreCards
           data={DATA}
-          onSwipteRight
-          onSwipeLeft
+          renderNoMoreCards={this.renderNoMoreCards}
+          // onSwipeRight
+          // onSwipeLeft
         />
       </View>
     );
@@ -34,6 +53,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 20,
     flex: 1,
     backgroundColor: '#fff'
   }
@@ -43,41 +63,49 @@ const DATA = [
   {
     id: 1,
     text: 'Card #1',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg'
+    uri:
+      'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg'
   },
   {
     id: 2,
     text: 'Card #2',
-    uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg'
+    uri:
+      'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg'
   },
   {
     id: 3,
     text: 'Card #3',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg'
+    uri:
+      'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg'
   },
   {
     id: 4,
     text: 'Card #4',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg'
+    uri:
+      'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg'
   },
   {
     id: 5,
     text: 'Card #5',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg'
+    uri:
+      'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg'
   },
   {
     id: 6,
     text: 'Card #6',
-    uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg'
+    uri:
+      'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg'
   },
   {
     id: 7,
     text: 'Card #7',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg'
+    uri:
+      'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg'
   },
   {
     id: 8,
     text: 'Card #8',
-    uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg'
+    uri:
+      'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg'
   }
 ];
